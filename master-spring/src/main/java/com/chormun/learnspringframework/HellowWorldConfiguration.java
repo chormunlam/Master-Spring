876b541e3aead58643932f64007e988fdb4977b6
Chormun <chormunlam@yahoo.com.hk>
2023-09-12 18:12:06 -0400

    how to using beans

--- master-spring/src/main/java/com/chormun/learnspringframework/HellowWorldConfiguration.java
+++ master-spring/src/main/java/com/chormun/learnspringframework/HellowWorldConfiguration.java
@@ -2,7 +2,7 @@ package com.chormun.learnspringframework;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-record Person(String name, int age ){ };
+record Person(String name, int age, Address address ){ };
 record Address(String firstLine, String city){};
 @Configuration
 public class HellowWorldConfiguration {
@@ -13,17 +13,32 @@ public class HellowWorldConfiguration {
 
     @Bean
     public int age(){
-        return 15;
+        return 33;
     }
 
     @Bean
     public Person person(){
-        return new Person("bobo", 5);
+        return new Person("bobo", 5, new Address("live with mom", "NYC"));
 
     }
+
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(), address());
+    }
+    @Bean
+    public Person person3Parameter(String name, int age, Address address3)//name, age, addrs2
+    {
+        return new Person(name, age, address3);
+    }
+    @Bean(name="address2")
     public  Address address(){
         return new Address("Baker st", "London");
+
+    }
+    @Bean(name="address3")
+    public  Address address3(){
+        return new Address("b612", "b612");
 
     }
 

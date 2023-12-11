c236e5402e06ccb75936e3039c24230ba63d4437
Chormun <chormunlam@yahoo.com.hk>
2023-12-11 15:04:39 -0500

    path parameter and @PathVariable for mapping

---
+++ restful-web-services/src/main/java/com/example/restfulwebservices/helloworld/HellowWorldBean.java
@@ -0,0 +1,25 @@
+package com.example.restfulwebservices.helloworld;
+
+public class HellowWorldBean {
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+    public HellowWorldBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HellowWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}

8a2770a7802620ef037b306fde4b59ebd7894378
Chormun <chormunlam@yahoo.com.hk>
2023-12-23 18:24:43 -0500

    react route review

--- 41-react/front-end/todo-app/src/components/todo/TodoApp.jsx
+++ 41-react/front-end/todo-app/src/components/todo/TodoApp.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import "./TodoAPP.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+  Link,
+} from "react-router-dom";
 
 export default function TodoApp() {
   return (
-    <>
-      <div>Todo app mamagerment</div>
-      <div>
-        <LoginComponent />
-      </div>
-      <div>{/* <WelcomeComponent /> */}</div>
-    </>
+    <div className="TodoApp">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome/:username" element={<WelcomeComponent />} />
+
+          <Route path="/todos" element={<ListTodosComponent />} />
+          <Route path="/logout" element={<LogoutComponent />} />
+          <Route path="*" element={<ErrorComponent />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
@@ -29,6 +45,8 @@ function LoginComponent() {
   const [showSucess, setShowSucess] = useState(false);
   const [showError, setShowError] = useState(false);
 
+  const navigate = useNavigate();
+
   function handleSubmit() {
     // console.log(pw);
     // console.log(username);
@@ -37,6 +55,7 @@ function LoginComponent() {
       console.log("sucess");
       setShowSucess(true);
       setShowError(false);
+      navigate(`/welcome/${username}`);
     } else {
       console.log("fail");
       setShowError(true);
@@ -46,6 +65,7 @@ function LoginComponent() {
 
   return (
     <div className="Login">
+      <h1>Login here</h1>
       {showSucess && (
         <div className="sucessMessage"> Authenticated Succesfully</div>
       )}
@@ -83,5 +103,104 @@ function LoginComponent() {
   );
 }
 function WelcomeComponent() {
-  return <div>Welcome Component </div>;
+  const { username } = useParams();
+  console.log(username);
+  return (
+    <div className="WelcomeComponent">
+      <h1>Welcome ~ {username}</h1>
+      <div>
+        mananger yourtodos. -<Link to="/todos">go here</Link>
+      </div>
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>sorry for the 404, reach out to our tema at xx</h1>{" "}
+    </div>
+  );
+}
+
+function ListTodosComponent() {
+  const today = new Date();
+
+  const targetDate = new Date(
+    today.getFullYear() + 12,
+    today.getMonth(),
+    today.getDay()
+  );
+  const todos = [
+    {
+      id: 1,
+      desc: "Learn AWS",
+      done: false,
+      targetDate: targetDate,
+    },
+    {
+      id: 2,
+      desc: "Learn full",
+      done: false,
+      targetDate: targetDate,
+    },
+    {
+      id: 3,
+      desc: "Learn Cloud",
+      done: false,
+      targetDate: targetDate,
+    },
+  ];
+  return (
+    <div className="ListTodosComponent">
+      <h1>things you want to have fun with</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Id</td>
+              <td>Desc</td>
+              <td>Is Done?</td>
+              <td>Target Date</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.desc}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="Header">
+      header <hr />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <hr /> Footer
+    </div>
+  );
+}
+
+function LogoutComponent() {
+  return (
+    <div className="logout">
+      <h1>logged out</h1>{" "}
+    </div>
+  );
 }

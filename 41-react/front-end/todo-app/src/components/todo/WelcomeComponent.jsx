fdaef473a1907f50677063b4f203b8b3b0d8dbe4
Chormun <chormunlam@yahoo.com.hk>
2023-12-24 14:31:03 -0500

    add todo backend

--- 41-react/front-end/todo-app/src/components/todo/WelcomeComponent.jsx
+++ 41-react/front-end/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,43 @@
 import { Link, useParams } from "react-router-dom";
-
+import { useState } from "react";
+import {
+  retrieveHelloWorldBean,
+  retrieveHelloWorldPathVar,
+} from "./api/HelloWorldApiService";
 export default function WelcomeComponent() {
   const { username } = useParams();
+
+  const [message, setMessage] = useState(null);
+
+  function callHelloWorldApi() {
+    console.log("called");
+    //axios
+    // axios
+    //   .get("http://localhost:8080/hello-world")
+    //   .then((res) => successResponse(res))
+    //   .catch((err) => errorResponse(err))
+    //   .finally(() => console.log("clean up"));
+
+    // retrieveHelloWorldBean()
+    //   .then((res) => successResponse(res))
+    //   .catch((err) => errorResponse(err))
+    //   .finally(() => console.log("clean up"));
+
+    //
+    retrieveHelloWorldPathVar("chormunApi")
+      .then((res) => successResponse(res))
+      .catch((err) => errorResponse(err))
+      .finally(() => console.log("clean up"));
+  }
+
+  function successResponse(res) {
+    console.log(res);
+    setMessage(res.data.message);
+  }
+
+  function errorResponse(err) {
+    console.log("errrrrrr: " + err);
+  }
 
   return (
     <div className="WelcomeComponent">
@@ -9,6 +45,12 @@ export default function WelcomeComponent() {
       <div>
         mananger yourtodos. -<Link to="/todos">go here</Link>
       </div>
+      <div>
+        <button className="btn btn-success" onClick={callHelloWorldApi}>
+          call hello world rest api
+        </button>
+      </div>
+      <div className="text-info">{message}</div>
     </div>
   );
 }

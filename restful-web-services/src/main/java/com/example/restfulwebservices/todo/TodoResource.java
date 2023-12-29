c7c1e4e0a14ccf0e7a7e0b3771b2cb48450de7ad
Chormun <chormunlam@yahoo.com.hk>
2023-12-29 16:00:04 -0500

    junit

--- restful-web-services/src/main/java/com/example/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/example/restfulwebservices/todo/TodoResource.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
 public class TodoResource {
 	
 	private TodoService todoService;
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, description, done, target_date, username)
+values (10001, 'learn databases', false, CURRENT_DATE(), 'cc');
+
+insert into todo(id, description, done, target_date, username)
+values(10002, 'learn databases2', false, CURRENT_DATE(), 'cc');
+
+insert into todo(id, description, done, target_date, username)
+values(10003, 'learn databases3', false, CURRENT_DATE(), 'cc');
